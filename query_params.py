6bf7bc00d931948c9102d6e48844295cd5afa0e3
issacsonjj <issacsonjj@gmail.com>
2017-03-04 09:23:36 +0800

    query param demo

--- query_params.py
+++ query_params.py
@@ -39,20 +39,16 @@ class Company(graphene.ObjectType):
     birthday = MyDateTime()
     name = graphene.String()
     license = graphene.String()
-    employees = graphene.List(Employee, id=graphene.Int())
-
-    def resolve_company(self, args, context, info):
-        print('args: {0}'.format(args))
-        return self.employees
-
-
-
+    employees = graphene.List(Employee)
 
 class Query(graphene.ObjectType):
-    company = graphene.Field(Company)
+    company = graphene.Field(Company, gender=graphene.String())
     def resolve_company(self, args, context, info):
-        print('args: {0}'.format(args))
+        print('query args: {0}'.format(args))
         employees = [Employee(id=1, name="name1", age=44, gender="male", dept="sales"), Employee(id=2, name="name2", age=44, gender="female", dept="R&D")]
+        gender = args.get("gender")
+        if gender:
+            employees = filter(lambda e: e.gender==gender, employees) 
         
         return Company(birthday=datetime.datetime.now(), name="gs", license="0000000", employees=employees)
 
@@ -62,6 +58,6 @@ app.add_url_rule("/", view_func=GraphQLView.as_view('graphql', schema=graphene.S
 
 
 if __name__ == '__main__':
-    # curl -H 'Content-type: application/graphql' -d 'query {company   {birthday, name, license, employees (id: 1) { age  }}}' http://localhost:5000/
-    # {"data":{"company":{"birthday":"2017-03-03T20:38:07.285602","name":"gs","license":"0000000","employees":[{"age":44},{"age":44}]}}}
+    # curl -H 'Content-type: application/graphql' -d 'query {company (gender: "male")  {birthday, name, license, employees { age, gender, name  }}}' http://localhost:5000/
+    # {"data":{"company":{"birthday":"2017-03-04T09:22:31.486064","name":"gs","license":"0000000","employees":[{"age":44,"gender":"male","name":"name1"}]}}}
     app.run()
